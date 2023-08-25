12d1d21bbc5a8ba83bbf05e7163bc9be16b7e047
demotedC0der <andri.kuka86@gmail.com>
2023-08-25 22:34:27 +0300

    updated

---
+++ OutProxy (Scraper)/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace OutProxy__Scraper_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.CheckBox4 = new System.Windows.Forms.CheckBox();
+            this.CheckBox3 = new System.Windows.Forms.CheckBox();
+            this.CheckBox2 = new System.Windows.Forms.CheckBox();
+            this.CheckBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(486, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(449, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "_";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.Color.Gold;
+            this.Panel1.Controls.Add(this.CheckBox4);
+            this.Panel1.Controls.Add(this.CheckBox3);
+            this.Panel1.Controls.Add(this.CheckBox2);
+            this.Panel1.Controls.Add(this.CheckBox1);
+            this.Panel1.Controls.Add(this.groupBox1);
+            this.Panel1.Controls.Add(this.button1);
+            this.Panel1.Controls.Add(this.Panel2);
+            this.Panel1.Controls.Add(this.label1);
+            this.Panel1.Controls.Add(this.label2);
+            this.Panel1.Location = new System.Drawing.Point(0, -1);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(530, 272);
+            this.Panel1.TabIndex = 2;
+            this.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // CheckBox4
+            // 
+            this.CheckBox4.AutoSize = true;
+            this.CheckBox4.Font = new System.Drawing.Font("Bell MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBox4.Location = new System.Drawing.Point(319, 145);
+            this.CheckBox4.Name = "CheckBox4";
+            this.CheckBox4.Size = new System.Drawing.Size(123, 38);
+            this.CheckBox4.TabIndex = 7;
+            this.CheckBox4.Text = "Socks 5";
+            this.CheckBox4.UseVisualStyleBackColor = true;
+            this.CheckBox4.CheckedChanged += new System.EventHandler(this.CheckBox4_CheckedChanged);
+            // 
+            // CheckBox3
+            // 
+            this.CheckBox3.AutoSize = true;
+            this.CheckBox3.Font = new System.Drawing.Font("Bell MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBox3.Location = new System.Drawing.Point(148, 145);
+            this.CheckBox3.Name = "CheckBox3";
+            this.CheckBox3.Size = new System.Drawing.Size(123, 38);
+            this.CheckBox3.TabIndex = 6;
+            this.CheckBox3.Text = "Socks 4";
+            this.CheckBox3.UseVisualStyleBackColor = true;
+            this.CheckBox3.CheckedChanged += new System.EventHandler(this.CheckBox3_CheckedChanged);
+            // 
+            // CheckBox2
+            // 
+            this.CheckBox2.AutoSize = true;
+            this.CheckBox2.Font = new System.Drawing.Font("Bell MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBox2.Location = new System.Drawing.Point(319, 90);
+            this.CheckBox2.Name = "CheckBox2";
+            this.CheckBox2.Size = new System.Drawing.Size(175, 38);
+            this.CheckBox2.TabIndex = 5;
+            this.CheckBox2.Text = "Https (SSL)";
+            this.CheckBox2.UseVisualStyleBackColor = true;
+            this.CheckBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // CheckBox1
+            // 
+            this.CheckBox1.AutoSize = true;
+            this.CheckBox1.Font = new System.Drawing.Font("Bell MT", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBox1.Location = new System.Drawing.Point(148, 90);
+            this.CheckBox1.Name = "CheckBox1";
+            this.CheckBox1.Size = new System.Drawing.Size(102, 38);
+            this.CheckBox1.TabIndex = 4;
+            this.CheckBox1.Text = "Https";
+            this.CheckBox1.UseVisualStyleBackColor = true;
+            this.CheckBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Bookman Old Style", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(62, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 138);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Types";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(270, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 50);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Scrape";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Panel2
+            // 
+            this.Panel2.BackColor = System.Drawing.Color.Gold;
+            this.Panel2.Location = new System.Drawing.Point(1, 3);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(455, 56);
+            this.Panel2.TabIndex = 2;
+            this.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseDown);
+            this.Panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseMove);
+            this.Panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 270);
+            this.Controls.Add(this.Panel1);
+            this.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "OutProxy (Scraper)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel Panel1;
+        private System.Windows.Forms.Panel Panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox CheckBox4;
+        private System.Windows.Forms.CheckBox CheckBox3;
+        private System.Windows.Forms.CheckBox CheckBox2;
+        private System.Windows.Forms.CheckBox CheckBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ OutProxy (Scraper)/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace OutProxy__Scraper_
+{
+    public partial class Form1 : Form
+    {
+        
+
+        int MouseinX = 0, MouseinY = 0;
+        int MouseX = 0, MouseY = 0;
+        bool mouseDown;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                MouseX = MousePosition.X - MouseinX;
+                MouseY = MousePosition.Y - MouseinY;
+
+                this.SetDesktopLocation(MouseX, MouseY);
+            }
+        }
+
+        private void Panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback = (snder, cert, chain, error) => true;
+            if (CheckBox1.Checked)
+            {
+                Directory.CreateDirectory("Scraped Proxies");
+                string contents = new WebClient().DownloadString("https://api.proxyscrape.com?request=displayproxies&proxytype=http&ssl=no");
+                Console.WriteLine(contents);
+                System.IO.File.WriteAllText("Scraped Proxies\\HTTP Proxies.txt", contents);
+                MessageBox.Show("Done! ");
+                Console.ReadLine();
+            }
+            if(CheckBox2.Checked)
+            {
+                Directory.CreateDirectory("Scraped Proxies");
+                string contents = new WebClient().DownloadString("https://api.proxyscrape.com?request=displayproxies&proxytype=http&ssl=yes");
+                Console.WriteLine(contents);
+                System.IO.File.WriteAllText("Scraped Proxies\\HTTP (SSL) Proxies.txt", contents);
+                MessageBox.Show("Done! ");
+                Console.ReadLine();
+            }
+            if (CheckBox3.Checked)
+            {
+                Directory.CreateDirectory("Scraped Proxies");
+                string contents = new WebClient().DownloadString("https://api.proxyscrape.com?request=displayproxies&proxytype=socks4");
+                Console.WriteLine(contents);
+                System.IO.File.WriteAllText("Scraped Proxies\\Socks 4 Proxies.txt", contents);
+                MessageBox.Show("Done! ");
+                Console.ReadLine();
+            }
+            if(CheckBox4.Checked)
+            {
+                Directory.CreateDirectory("Scraped Proxies");
+                string contents = new WebClient().DownloadString("https://api.proxyscrape.com?request=displayproxies&proxytype=socks5");
+                Console.WriteLine(contents);
+                System.IO.File.WriteAllText("Scraped Proxies\\Socks 5 Proxies.txt", contents);
+                MessageBox.Show("Done! ");
+                Console.ReadLine();
+
+            }
+
+            else if(!CheckBox4.Checked && !CheckBox3.Checked && !CheckBox2.Checked && !CheckBox1.Checked)
+            {
+                MessageBox.Show("Error");
+                
+            }
+           
+
+        }
+
+        private void CheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            MouseinX = MousePosition.X - Bounds.X;
+            MouseinY = MousePosition.Y - Bounds.Y;
+
+        }
+    }
+}
